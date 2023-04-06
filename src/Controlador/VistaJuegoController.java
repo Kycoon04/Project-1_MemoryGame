b845c749df0f5d1457083b2c11aabb5fa8551614
Kycoon <jomaval4@gmail.com>
2023-04-06 10:07:14 -0600

    cambio

--- src/Controlador/VistaJuegoController.java
+++ src/Controlador/VistaJuegoController.java
@@ -513,7 +513,7 @@ public class VistaJuegoController implements Initializable {
     Dificil.CargarNumero();
     Dificil.CargarImagen();
     if(CartaComodin.isSelected()){
-         Dificil.CartaComodin();
+        Dificil.CartaComodin();
     }
     Dificil.Iniciar(VistaFinalizar, Tabla,TiempoMaxTotal.getValue(),TiempoMaxJugador.getValue());
     }
