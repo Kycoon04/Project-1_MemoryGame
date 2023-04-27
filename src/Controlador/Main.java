f76c1e20e10073e86bee3d83c72f06f02a8d166c
Kycoon <jomaval4@gmail.com>
2023-04-27 14:20:35 -0600

    cambio

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
       try {
+          
                 Parent root = FXMLLoader.load(getClass().getResource("/Vista/VistaJuego.fxml"));
                 Scene scene = new Scene(root);
                 primaryStage.setScene(scene);
